42819e811750e7cb449bd5edf49e2ad120550bd8
Conan Tzou <conan@asperasoft.com>
2018-01-20 17:30:18 -0800

    add footer

--- src/modules/layout/index.js
+++ src/modules/layout/index.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 import { states } from './layout.states';
-import './layout.scss';
+import './header.scss';
+import './footer.scss';
 
 export default angular.module('rqhlaw.layout', [uiRouter])
   .config(states)
